57fd014e0f81eddd438a471149a2b5eb44824e72
matheuefranco <matheusefranco@gmail.com>
2025-08-22 08:21:48 -0300

    first commit

---
+++ ExemploAula/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MinhaBiblioteca;
+
+class Program
+{
+    static double mediaValoresVetor(int[] array)
+    {
+        double soma = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            soma += array[i];
+        }
+        return soma / array.Length;
+    }
+
+    static void Main()
+    {
+        int n;
+        double media;
+        Console.WriteLine("Tamanho do vetor:");
+        n = int.Parse(Console.ReadLine());
+        int[] vetor = new int[n];
+        Biblioteca.gerarVetor(vetor);
+        Biblioteca.mostrarVetor(vetor);
+        media = mediaValoresVetor(vetor);
+        Console.WriteLine($"Media dos valores do vetor: {media:F2}");
+    }
+
+
+}
